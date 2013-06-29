9902622e7014e88f39340aab681cc185856cb821
Isaac <ikecollege@gmail.com>
2013-06-29 15:09:03 -0500

    Add menu path, iframe menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Wtw::Application.routes.draw do
 
    root to: 'static_pages#home'
    get '/location' => 'static_pages#location'
+   get '/menu'     => 'static_pages#menu'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
