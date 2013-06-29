87a5be67bac5a317d63252d52509b544624cb703
Isaac <ikecollege@gmail.com>
2013-06-29 16:24:34 -0500

    Link to menu at google

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,6 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def menu
-  end
-
   def location
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Wtw::Application.routes.draw do
 
    root to: 'static_pages#home'
    get '/location' => 'static_pages#location'
-   get '/menu'     => 'static_pages#menu'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
