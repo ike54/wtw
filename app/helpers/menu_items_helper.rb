eb7258546b8f24c2b64dc8931c3f191d8362a2f9
Isaac <ikecollege@gmail.com>
2013-07-06 02:23:43 -0500

    add meta description to west texas wings

--- app/helpers/menu_items_helper.rb
+++ app/helpers/menu_items_helper.rb
@@ -1,17 +1,36 @@
 module MenuItemsHelper
-	def listItemsInSection(menu, section)
-		#html = content_tag(:h2, section)
 
-		   @menu.each do |item|
-		    	list = content_tag(:dt, 'stuff')
-		    	#list += content_tag(:dd, item.description)
-		    	#list += content_tag(:dd, item.price)
-		    end
-		    #list += content_tag(:dl, list)
-	    list	   
+    def make_menu
 
+     	sections = []
+    	@menu.each do |item|
+    		sections.push item.section
+    	end
+    	sections = sections.uniq
 
+        html = "".html_safe
+    	sections.each do |section|
+    		html.safe_concat "<h2>" + section + "</h2>"
+    		html.safe_concat "<dl>"
+    		x = @menu.find(:all, :conditions => {:section  => section})
+    		x.each do |item|
+    			html.safe_concat "<dt>" + item.item
 
-		#html 
+    			if item.price == nil
+    				html.safe_concat "</dt>"
+    			else
+    				html.safe_concat " - " + item.price + "</dt>"
+    			end
+    			if item.description == nil
+    				html.safe_concat ""
+    			else
+    				html.safe_concat "<dd>" + item.description + "</dd>"
+    			end
+    			html.safe_concat "<br />"		
+    		end
+    		html.safe_concat "</dl>" 
+    	end
+
+    	html
     end
 end
